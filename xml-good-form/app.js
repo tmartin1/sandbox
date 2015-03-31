a7f1f09d07091b594416ddc62db433482e880aff
Tim Martin <timothy.m.martin@gmail.com>
2015-03-31 12:53:11 -0700

    Add xml toy problem.

---
+++ xml-good-form/app.js
@@ -0,0 +1,8 @@
+// Given a string of XML code, write a function that check to make sure the code is in good form, meaning
+// that every open tag has an appropriate closing tag and that they are all in the correct order.
+// Assume that the input is always a non-empty string. Your function should return a boolean.
+
+//
+var checkXml = function(str) {
+  //
+};

21e7871af4536b97207d808ebf82a2b656ae4fe2
Tim Martin <timothy.m.martin@gmail.com>
2015-04-03 16:14:15 -0700

    Cleaned up and fixed functions.

--- pig-latin/app.js
+++ pig-latin/app.js
@@ -1,25 +1,29 @@
-// This was the coding challenge from the onsite interview, plugged my computer into a large monitor
-// and I explained what I was doing and why as I was coding.
-
+// Translated text that is input into the textarea box and displays it under the 'Translated Text' section.
 var translateText = function() {
-  $('#posttranslated').text(pigLatin($('#pretranslated').val()));
+  $('#posttranslated').html(pigLatin($('#pretranslated').val()));
 };
 
-// Assume input is always a string.
+// Assumes that the input is always a string. Returns translated string.
 var pigLatin = function(str) {
-  str = str.split(' ');
-  // end of word is when there is a non-alphabetic character (also not apost.)
-  for (var i=0, n=str.length; i<n; i++) {
-    str[i] = translate(str[i]);
+  var paragraphs = str.split('\n');
+  if (paragraphs.length > 1) {
+    for (var i=0, n=paragraphs.length; i<n; i++) {
+      paragraphs[i] = pigLatin(paragraphs[i]);
+    }
+    return paragraphs.join('<br>');
+  } else {
+    str = str.split(' ');
+    for (var i=0, n=str.length; i<n; i++) {
+      str[i] = translate(str[i]);
+    }
+    return str.join(' ');
   }
-  return str.join(' ');
 };
 
 // Accept single word as parameter, return translated word.
 var translate = function(word) {
-  var wordLength = word.length;
-
-  // Check for hyphens in word
+  if (word === '') return word;
+  // Check for and address hyphens in the word.
   var subs = word.split('-');
   if (subs.length > 1) {
     for (var i=0, n=subs.length; i<n; i++) {
@@ -28,54 +32,43 @@ var translate = function(word) {
     return subs.join('-');
   }
 
+  // Remove punctuation and set to lower case.
+  var result = word.replace(/[^a-z0-9]/ig, '').toLowerCase();
+  var length = result.length;
+  if (result.match(/[^a-z0-9]/gi)) return word;
+
   // If word ends in 'way' don't modify.
-  if (wordLength >= 3 && word.substring(wordLength-3, wordLength) === 'way') {
-    return word;
+  // If word starts with vowel: move first letter to end and add 'way'.
+  // If word starts with consonant (or non-alpha): move first letter to end and add 'ay'.
+  if (result.substring(length-3, length) !== 'way') {
+    if (result[0].match(/[aeiou]/i)) result += 'way';
+    else result = result.substring(1, length) + result[0] + 'ay';
   }
 
-  // Check for punctuation, preserve location relative to end of word.
-  var punctuation = [];
-  for (var i=0, n=wordLength; i<n; i++) {
+  // Check for capitals and punctuation. Match style if needed.
+  if (word.match(/[A-Z]/g)) result = matchCaps(word, result);
+  if (word.match(/[^a-z0-9]/ig)) result = matchPunctuation(word, result);
+
+  return result;
+};
+
+// Capitalizes the translated word to match the original word format (from front).
+var matchCaps = function(word, result) {
+  result = result.split('');
+  for (var i=0, n=word.length; i<n; i++) {
+    if (word[i].match(/[A-Z]/)) result[i] = result[i].toUpperCase();
+  }
+  return result.join('');
+};
+
+// Adds punctuation to match the original word format (from end).
+var matchPunctuation = function(word, result) {
+  var wLength = word.length - 1, rLength = result.length - 1;
+  for (var i=wLength, r=rLength, shift=1; i>=0; i--, r--) {
     if (word[i].match(/[^a-z0-9]/i)) {
-      punctuation.push([ word[i], wordLength - i - 1 ]);
+      result = result.substring(0, r+shift) + word[i] + result.substring(r+shift, rLength+shift);
+      shift++;
     }
   }
-  word = word.replace(/[^a-z0-9]/ig, '');
-  wordLength = word.length;
-
-  // Check for capitalization, preserve location relative to start of word.
-  var caps = [];
-  for (var i=0, n=wordLength; i<n; i++) {
-    if (word[i].match(/[A-Z]/)) {
-      caps.push(i);
-    }
-  }
-  word = word.toLowerCase();
-
-  // If word starts with vowel
-  if (word[0].match(/[aeiou]/i)) {
-    word += 'way';
-  }
-
-  // If word starts with consonant (or non-alpha)
-  else {
-    word = word.substring(1, wordLength) + word[0] + 'ay';
-  }
-
-  wordLength = word.length;
-
-  // Correct for capitalization.
-  word = word.split('');
-  for (var i=0, n=caps.length; i<n; i++) {
-    word[i] = word[i].toUpperCase();
-  }
-  word = word.join('');
-
-  // Correct for punctuation.
-  for (var i=0, n=punctuation.length; i<n; i++) {
-    var temp = punctuation[i];
-    word = word.substring(0, wordLength - temp[1]) + temp[0] + word.substring(wordLength - temp[1], word.length);
-  }
-
-  return word;
+  return result;
 };
